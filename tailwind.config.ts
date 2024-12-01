0e29eba2b4c1c5a16799cc35d17d98e905e09b7c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-12-01 12:43:00 +0900

    chore: daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'tailwindcss';
 import containerQueries from '@tailwindcss/container-queries';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
@@ -10,5 +11,9 @@ export default {
 		extend: {},
 	},
 
-	plugins: [typography, forms, containerQueries],
+	plugins: [typography, forms, containerQueries, daisyui],
+	daisyui: {
+		themes: ['cupcake'],
+		logs: false,
+	},
 } satisfies Config;
